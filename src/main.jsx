b8adf96e1abb907e059821f8cc2ee54ff77e045c
Victor3294 <vblum26@gmail.com>
2024-09-03 19:36:36 -0300

    feat: 🎸 Resolução do exercicio 05

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
+import { CycleProvider } from './contexts/cycle.jsx'
 
 createRoot(document.getElementById('root')).render(
     <AuthProvider>
-        <App />
+        <CycleProvider>
+            <App />
+        </CycleProvider>
     </AuthProvider>
 )
